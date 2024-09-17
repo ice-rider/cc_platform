7bd1da98cf16f123892a9143f91dec9f858238ea
codevoke <119005542+codevoke@users.noreply.github.com>
2024-09-17 04:50:59 +0000

    updates

--- cc_platform/src/desktop/components/__init__.jsx
+++ cc_platform/src/desktop/components/__init__.jsx
@@ -1,6 +1,5 @@
 import AuthHeaderView from "./AuthHeaderView";
 import Header from "./Header";
 import Link from "./Link";
-import FollowInfo from "./FollowInfo";
 
-export { AuthHeaderView, Header, Link, FollowInfo };
+export { AuthHeaderView, Header, Link };
